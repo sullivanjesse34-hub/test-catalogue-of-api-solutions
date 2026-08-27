8713a8673d638435ba3db1ed95f725f3e88948f5
jessemsullivan <sullivan.jesse34@gmail.com>
2026-08-27 16:07:09 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/test-catalogue-of-api-solutions',
+  images: {unoptimized: true},
 };
 
 export default nextConfig;
